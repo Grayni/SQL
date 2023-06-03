ba68d32b42f90b5d8e2efca57fb9ecea63e40aed
Grayni <grayniweb@gmail.com>
2023-06-03 07:35:04 +0300

    Project Colonization

---
+++ colonization/data.sql
@@ -0,0 +1,142 @@
+INSERT INTO `astronauts` (firstname, lastname, gender, birthday, nationality, colony_id, skils) VALUES
+	('John', 'Smith', 'Male', '1990-01-15', 'USA', 1, 'Expert in Mars colonization research and robotics'),
+    ('Emma', 'Johnson', 'Female', '1988-05-23', 'Canada', 2, 'Skilled in medical research for long-duration space missions'),
+    ('Michael', 'Williams', 'Male', '1985-07-10', 'Australia', 1, 'Proficient in Mars geology and geological sample analysis'),
+    ('Olivia', 'Brown', 'Female', '1992-02-08', 'USA', 3, 'Specialist in Mars agriculture and sustainable food production'),
+    ('James', 'Jones', 'Male', '1983-11-17', 'UK', 2, 'Experienced in Mars habitat design and construction'),
+    ('Emily', 'Davis', 'Female', '1986-09-29', 'USA', 1, 'Expert in Mars atmospheric research'),
+    ('Alexander', 'Taylor', 'Male', '1987-06-12', 'Russia', 2, 'Skilled in Mars rover operations and exploration'),
+    ('Sophia', 'Anderson', 'Female', '1984-04-05', 'Germany', 3, 'Specialist in space medicine and human physiology'),
+    ('Daniel', 'Miller', 'Male', '1989-03-21', 'USA', 1, 'Proficient in Mars resource utilization and ISRU technologies'),
+    ('Ava', 'Garcia', 'Female', '1995-12-19', 'Mexico', 2, 'Experienced in Mars mission planning and logistics'),
+    ('Liam', 'Wilson', 'Male', '1988-08-14', 'USA', 1, 'Expert in Mars climate modeling and simulations'),
+    ('Mia', 'Martinez', 'Female', '1991-03-03', 'Spain', 2, 'Skilled in astrobiology and extremophile research'),
+    ('Benjamin', 'Lee', 'Male', '1983-12-27', 'South Korea', 3, 'Proficient in Mars surface operations and exploration'),
+    ('Charlotte', 'Clark', 'Female', '1994-07-08', 'USA', 1, 'Specialist in space agriculture and hydroponics systems'),
+    ('Henry', 'Rodriguez', 'Male', '1987-05-11', 'Mexico', 2, 'Experienced in space robotics and autonomous systems'),
+    ('Harper', 'Walker', 'Female', '1990-01-19', 'USA', 3, 'Expert in Mars geological sample collection and analysis'),
+    ('Ethan', 'Hill', 'Male', '1985-09-07', 'Canada', 1, 'Skilled in space medicine and telemedicine'),
+    ('Amelia', 'Baker', 'Female', '1986-03-15', 'UK', 2, 'Specialist in Mars habitat life support systems'),
+    ('Sebastian', 'Fisher', 'Male', '1992-11-28', 'Germany', 1, 'Proficient in Mars geophysical research'),
+    ('Lily', 'Morris', 'Female', '1989-04-25', 'Australia', 2, 'Experienced in Mars mission simulation and training'),
+    ('Noah', 'Lewis', 'Male', '1993-02-18', 'USA', 3, 'Expert in Mars mission communication and data systems'),
+    ('Grace', 'Young', 'Female', '1984-10-05', 'Canada', 1, 'Skilled in astrochemistry and planetary formation'),
+    ('David', 'Turner', 'Male', '1986-06-27', 'Australia', 2, 'Proficient in space physics and magnetospheric research'),
+    ('Victoria', 'Adams', 'Female', '1990-12-10', 'USA', 1, 'Specialist in Mars exploration rover operations'),
+    ('Samuel', 'Wright', 'Male', '1987-08-29', 'UK', 3, 'Experienced in space habitats and life support systems'),
+    ('Ella', 'Harris', 'Female', '1991-05-17', 'USA', 2, 'Expert in Mars sample return missions'),
+    ('Matthew', 'Cooper', 'Male', '1988-09-03', 'Russia', 1, 'Skilled in space navigation and trajectory analysis'),
+    ('Scarlett', 'Barnes', 'Female', '1985-07-21', 'Canada', 3, 'Specialist in space radiation shielding and protection'),
+    ('Leo', 'Allen', 'Male', '1989-03-14', 'Germany', 2, 'Proficient in Mars dust and regolith research'),
+    ('Stella', 'Parker', 'Female', '1994-11-22', 'USA', 1, 'Experienced in space suit design and testing'),
+    ('Daniel', 'Walker', 'Male', '1989-06-11', 'USA', 2, 'Expert in Mars surface exploration and mapping'),
+    ('Chloe', 'Gonzalez', 'Female', '1986-03-02', 'Spain', 3, 'Skilled in astrogeology and planetary mapping'),
+    ('Jacob', 'Hall', 'Male', '1983-11-25', 'Canada', 1, 'Proficient in space biology and life sciences'),
+    ('Zoe', 'White', 'Female', '1992-08-12', 'USA', 2, 'Specialist in Mars habitat maintenance and repair'),
+    ('Gabriel', 'Scott', 'Male', '1985-04-21', 'UK', 3, 'Experienced in Mars mission simulations and analog studies'),
+    ('Sofia', 'Lee', 'Female', '1990-02-09', 'USA', 1, 'Expert in space weather and solar radiation'),
+    ('Carter', 'Lopez', 'Male', '1987-09-07', 'Russia', 2, 'Skilled in Mars geological mapping and analysis'),
+    ('Avery', 'Adams', 'Female', '1984-06-15', 'Canada', 3, 'Specialist in space agriculture and plant cultivation'),
+    ('Owen', 'Morris', 'Male', '1989-03-21', 'Germany', 1, 'Proficient in space debris mitigation and tracking'),
+    ('Luna', 'Rivera', 'Female', '1994-12-19', 'USA', 2, 'Experienced in Mars habitat testing and validation'),
+    ('Liam', 'Martin', 'Male', '1993-02-18', 'USA', 3, 'Expert in Mars mission communication and data systems'),
+    ('Emma', 'Lee', 'Female', '1984-10-05', 'Canada', 1, 'Skilled in astrochemistry and planetary formation'),
+    ('Noah', 'Garcia', 'Male', '1986-06-27', 'Australia', 2, 'Proficient in space physics and magnetospheric research'),
+    ('Olivia', 'Clark', 'Female', '1990-12-10', 'USA', 1, 'Specialist in Mars exploration rover operations'),
+    ('Ava', 'Wright', 'Female', '1987-08-29', 'UK', 3, 'Experienced in space habitats and life support systems'),
+    ('Sophia', 'Hall', 'Female', '1991-05-17', 'USA', 2, 'Expert in Mars sample return missions'),
+    ('Jackson', 'Scott', 'Male', '1988-09-03', 'Russia', 1, 'Skilled in space navigation and trajectory analysis'),
+    ('Mia', 'Adams', 'Female', '1985-07-21', 'Canada', 3, 'Specialist in space radiation shielding and protection'),
+    ('Aiden', 'Morris', 'Male', '1989-03-14', 'Germany', 2, 'Proficient in Mars dust and regolith research'),
+    ('Charlotte', 'Parker', 'Female', '1994-11-22', 'USA', 1, 'Experienced in space suit design and testing'),
+    ('Liam', 'Martin', 'Male', '1993-02-18', 'USA', 3, 'Expert in Mars mission communication and data systems'),
+    ('Emma', 'Lee', 'Female', '1984-10-05', 'Canada', 1, 'Skilled in astrochemistry and planetary formation'),
+    ('Noah', 'Garcia', 'Male', '1986-06-27', 'Australia', 2, 'Proficient in space physics and magnetospheric research'),
+    ('Olivia', 'Clark', 'Female', '1990-12-10', 'USA', 1, 'Specialist in Mars exploration rover operations'),
+    ('Ava', 'Wright', 'Female', '1987-08-29', 'UK', 3, 'Experienced in space habitats and life support systems'),
+    ('Sophia', 'Hall', 'Female', '1991-05-17', 'USA', 2, 'Expert in Mars sample return missions'),
+    ('Jackson', 'Scott', 'Male', '1988-09-03', 'Russia', 1, 'Skilled in space navigation and trajectory analysis'),
+    ('Mia', 'Adams', 'Female', '1985-07-21', 'Canada', 3, 'Specialist in space radiation shielding and protection'),
+    ('Aiden', 'Morris', 'Male', '1989-03-14', 'Germany', 2, 'Proficient in Mars dust and regolith research'),
+    ('Charlotte', 'Parker', 'Female', '1994-11-22', 'USA', 1, 'Experienced in space suit design and testing'),
+    ('Daniel', 'Walker', 'Male', '1989-06-11', 'USA', 2, 'Expert in Mars surface exploration and mapping'),
+    ('Chloe', 'Gonzalez', 'Female', '1986-03-02', 'Spain', 3, 'Skilled in astrogeology and planetary mapping'),
+    ('Jacob', 'Hall', 'Male', '1983-11-25', 'Canada', 1, 'Proficient in space biology and life sciences'),
+    ('Zoe', 'White', 'Female', '1992-08-12', 'USA', 2, 'Specialist in Mars habitat maintenance and repair'),
+    ('Gabriel', 'Scott', 'Male', '1985-04-21', 'UK', 3, 'Experienced in Mars mission simulations and analog studies'),
+    ('Sofia', 'Lee', 'Female', '1990-02-09', 'USA', 1, 'Expert in space weather and solar radiation'),
+    ('Carter', 'Lopez', 'Male', '1987-09-07', 'Russia', 2, 'Skilled in Mars geological mapping and analysis'),
+    ('Avery', 'Adams', 'Female', '1984-06-15', 'Canada', 3, 'Specialist in space agriculture and plant cultivation'),
+    ('Owen', 'Morris', 'Male', '1989-03-21', 'Germany', 1, 'Proficient in space debris mitigation and tracking'),
+    ('Luna', 'Rivera', 'Female', '1994-12-19', 'USA', 2, 'Experienced in Mars habitat testing and validation'),
+    ('Elijah', 'Harris', 'Male', '1986-09-08', 'USA', 1, 'Skilled in space communications and satellite operations'),
+    ('Harper', 'Turner', 'Female', '1987-07-14', 'Canada', 2, 'Expert in Mars atmospheric studies and climate modeling'),
+    ('Sebastian', 'Collins', 'Male', '1992-05-19', 'Australia', 3, 'Proficient in space robotics and autonomous systems'),
+    ('Amelia', 'Bailey', 'Female', '1990-03-25', 'USA', 1, 'Specialist in extraterrestrial life detection and astrobiology'),
+    ('Grayson', 'Miller', 'Male', '1985-11-02', 'UK', 2, 'Experienced in Mars mission planning and logistics'),
+    ('Layla', 'Young', 'Female', '1993-08-11', 'USA', 3, 'Expert in space medicine and astronaut health monitoring'),
+    ('Henry', 'Flores', 'Male', '1988-12-07', 'Russia', 1, 'Skilled in Mars geology and rock sample analysis'),
+    ('Mila', 'Baker', 'Female', '1984-04-16', 'Canada', 2, 'Specialist in space radiation effects and protection'),
+    ('Jack', 'Gonzalez', 'Male', '1989-06-29', 'Germany', 3, 'Proficient in space propulsion systems and rocketry'),
+    ('Emily', 'Roberts', 'Female', '1991-02-23', 'USA', 1, 'Experienced in Mars surface operations and rover control'),
+    ('William', 'Hall', 'Male', '1987-09-18', 'UK', 2, 'Expert in space astrophysics and cosmology'),
+    ('Scarlett', 'Carter', 'Female', '1990-07-12', 'USA', 3, 'Skilled in Mars soil analysis and regolith studies'),
+    ('James', 'Murphy', 'Male', '1986-01-30', 'Australia', 1, 'Specialist in space mission simulations and training'),
+    ('Aria', 'Lopez', 'Female', '1988-10-26', 'Canada', 2, 'Proficient in space weather forecasting and prediction'),
+    ('Benjamin', 'Young', 'Male', '1983-12-14', 'USA', 3, 'Experienced in Mars habitat design and construction'),
+    ('Lily', 'Hill', 'Female', '1989-04-21', 'UK', 1, 'Expert in space materials science and engineering'),
+    ('Michael', 'Adams', 'Male', '1985-07-05', 'Germany', 2, 'Skilled in Mars geological field exploration'),
+    ('Grace', 'Morris', 'Female', '1992-06-30', 'USA', 3, 'Specialist in space navigation and orbital mechanics'),
+    ('Joseph', 'Cook', 'Male', '1987-09-24', 'Canada', 1, 'Proficient in space biology and biotechnology'),
+    ('Victoria', 'Ramirez', 'Female', '1994-03-02', 'USA', 2, 'Experienced in Mars mission control and operations'),
+    ('Ethan', 'Parker', 'Male', '1988-06-19', 'USA', 1, 'Skilled in Mars geophysics and planetary geodesy'),
+    ('Ariana', 'Reed', 'Female', '1991-04-23', 'Canada', 2, 'Expert in space mission coordination and logistics'),
+    ('Alexander', 'Bell', 'Male', '1985-11-15', 'Australia', 3, 'Proficient in space systems engineering and integration'),
+    ('Brooklyn', 'Collins', 'Female', '1990-01-08', 'USA', 1, 'Specialist in Mars surface geology and crater analysis'),
+    ('Mason', 'Foster', 'Male', '1987-09-27', 'UK', 2, 'Experienced in space biology and bioregenerative systems'),
+    ('Aurora', 'Howard', 'Female', '1992-08-04', 'USA', 3, 'Expert in Mars atmospheric entry and descent'),
+    ('Logan', 'Barnes', 'Male', '1986-05-11', 'Russia', 1, 'Skilled in space propulsion and rocket engine design'),
+    ('Nora', 'Simmons', 'Female', '1984-02-16', 'Canada', 2, 'Specialist in space debris tracking and mitigation'),
+    ('Lucas', 'Morgan', 'Male', '1989-07-30', 'Germany', 3, 'Proficient in Mars geological field mapping and analysis'),
+    ('Hazel', 'Ross', 'Female', '1991-03-26', 'USA', 1, 'Experienced in space life support systems and habitation'),
+    ('Matthew', 'Stewart', 'Male', '1987-10-01', 'UK', 2, 'Expert in space plasma physics and magnetospheric studies'),
+    ('Natalie', 'Sanders', 'Female', '1990-06-14', 'USA', 3, 'Skilled in Mars mission planning and operations'),
+    ('David', 'Turner', 'Male', '1985-12-27', 'Australia', 1, 'Specialist in space communications and satellite systems'),
+    ('Samantha', 'Ward', 'Female', '1988-03-07', 'Canada', 2, 'Proficient in space weather and solar activity monitoring'),
+    ('John', 'Harris', 'Male', '1983-11-11', 'USA', 3, 'Experienced in Mars rover operations and navigation'),
+    ('Leah', 'Cox', 'Female', '1989-09-24', 'UK', 1, 'Expert in space materials science and metallurgy'),
+    ('Christopher', 'Reid', 'Male', '1985-08-13', 'Germany', 2, 'Skilled in Mars atmospheric chemistry and analysis'),
+    ('Stella', 'Fisher', 'Female', '1992-07-18', 'USA', 3, 'Specialist in space navigation and trajectory optimization'),
+    ('Samuel', 'Murray', 'Male', '1987-12-09', 'Canada', 1, 'Proficient in space biology and astroecology'),
+    ('Olivia', 'Bryant', 'Female', '1991-05-22', 'USA', 2, 'Experienced in Mars habitat design and ergonomics');
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ colonization/start.sql
@@ -0,0 +1,7 @@
+/* Run all scripts */
+
+-- table initialization
+SOURCE tables.sql;
+
+-- add data in database
+SOURCE data.sql;
